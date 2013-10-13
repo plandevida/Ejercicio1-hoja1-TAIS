ec3f2eaa84d4e745cd5829ee0277b832bb3160bd
plandevida <plandevida@planes.com>
2013-10-13 20:52:24 +0200

    Initial Commit

---
+++ Ejercicio1-hoja1-TAIS/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Ejercicio1-hoja1-TAIS
+//
+//  Created by Pablo Pinochet on 13/10/13.
+//  Copyright (c) 2013 Plandevida. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
